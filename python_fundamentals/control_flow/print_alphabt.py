1961b447515f08801a946b855ea18b35a97821db
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-22 01:33:38 +0200

    Removed newline

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -2,16 +2,16 @@
 
 letter = 97
 
-while letter:
+while letter < 124:
     if letter == 101:
         letter += 1
         continue
     if letter == 113:
         letter += 1
         continue
-    if letter == 123:
-        letter = 10
+    # if letter == 123:
+    #     letter = 10
     print("{:c}".format(letter), end="")
-    if letter == 10:
-        break
+    # if letter == 10:
+    #     break
     letter += 1
