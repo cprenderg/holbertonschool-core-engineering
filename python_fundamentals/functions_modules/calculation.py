d8e26e8b49db36060174ddf7b76b0e6470637ffc
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-22 03:54:17 +0200

    Fixed print formatting again

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 if __name__ == "__main__":
-    print("{}".format(add(a, b)))
-    print("{}".format(sub(a, b)))
-    print("{}".format(mul(a, b)))
-    print("{}".format(div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
