1718b891034c5b35c0c1583bfbbd6cd7aed0edcc
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-22 02:55:27 +0200

    Fixed print formatting

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    upperstr = ""
+    letter = 0
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            upperstr += (chr(ord(c) - 32))
+            letter = ord(c) - 32
         else:
-            upperstr += c
-    print(upperstr)
+            letter = ord(c)
+        print("{:c}".format(letter), end="")
+    print("")
+
+uppercase("TeSt")
