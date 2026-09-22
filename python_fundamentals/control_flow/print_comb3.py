3d75195213f61b906934c53bbbd7fb9b2ce5aa9a
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-22 02:33:57 +0200

    complete changed logic and saved original idea as first_print_comb3.py

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 
-printed = []
-skip = 0
-
-for x in range(99):
-    skip = 0
-    for num in printed:
-        if x % 10 != int(x / 10):
-            if x % 10 == num % 10 or x % 10 == int(num / 10):
-                if int(x / 10) == int(num / 10) or int(x / 10) == num % 10:
-                    skip = 1
-        if x > 10 and x % 10 == 0:
-            skip = 1
-    if skip == 0:
+for x in range (90):
+    if int(x / 10) < x % 10:
         if x == 89:
             print("{:02d}".format(x))
         else:
             print("{:02d}, ".format(x), end="")
-        printed.append(x)
