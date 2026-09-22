15ce786b28017dfdef5e916148bc371ccb489432
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-22 03:48:00 +0200

    Added file

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+if __name__ == "__main__":
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
