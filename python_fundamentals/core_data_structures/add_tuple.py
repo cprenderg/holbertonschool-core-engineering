07f106f455c7320dcf4d71d242931512ad4f5af4
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-23 05:25:54 +0200

    Added file

---
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    arr = []
+    i = 0
+    if i < len(tuple_a):
+        while i < len(tuple_a):
+            if not tuple_a[i]:
+                x = 0
+            else:
+                x = tuple_a[i]
+            if i < len(tuple_b):
+                y = tuple_b[i]
+            else:
+                y = 0
+            arr.append(x + y)
+            i += 1
+    elif i < len(tuple_b):
+        if not tuple_b[i]:
+            y = 0
+        else:
+            y = tuple_b[i]
+        if i < len(tuple_a):
+            x = tuple_a[i]
+        else:
+            x = 0
+        arr.append(x + y)
+        i += 1
+    if arr:
+        return (arr[0], arr[1])
+    return (0, 0)
