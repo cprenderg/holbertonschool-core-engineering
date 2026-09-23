b09cd769b63dd922ef6c1a5553d00c9995412095
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-23 05:06:16 +0200

    actually added case for empty matrix v4

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print(" ")
-    else:
-        i = 0
-        k = 0
-        for column in matrix[0]:
-            k += 1
-        for row in matrix:
-            j = 0
-            while j < k:
-                if j == k - 1:
-                    print("{:d}".format(matrix[i][j]))
-                else:
-                    print("{:d} ".format(matrix[i][j]), end="")
-                j += 1
-            i += 1
-
+    i = 0
+    k = 0
+    for column in matrix[0]:
+        k += 1
+    for row in matrix:
+        j = 0
+        while j < k:
+            if j == k - 1:
+                print("{:d}".format(matrix[i][j]))
+            else:
+                print("{:d} ".format(matrix[i][j]), end="")
+            j += 1
+        i += 1
+    if k == 0:
+        print("")
