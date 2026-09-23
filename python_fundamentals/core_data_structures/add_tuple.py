96d63f4418006848ab4af31b75f3ac35db0f51d9
Christopher Prendergast <14800@holbertonstudents.com>
2026-09-23 06:25:53 +0200

    Changed logic to deal with tuple_a being shorter than tuple_b

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -3,7 +3,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     arr = []
     i = 0
-    if i < len(tuple_a):
+    if len(tuple_a) >= len(tuple_b) and i < len(tuple_a):
         while i < len(tuple_a):
             if not tuple_a[i]:
                 x = 0
@@ -15,17 +15,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 y = 0
             arr.append(x + y)
             i += 1
-    elif i < len(tuple_b):
-        if not tuple_b[i]:
-            y = 0
-        else:
-            y = tuple_b[i]
-        if i < len(tuple_a):
-            x = tuple_a[i]
-        else:
-            x = 0
-        arr.append(x + y)
-        i += 1
+    elif len(tuple_b) > len(tuple_a) and i < len(tuple_b):
+        while i < len(tuple_b):
+            if not tuple_b[i]:
+                y = 0
+            else:
+                y = tuple_b[i]
+            if i < len(tuple_a):
+                x = tuple_a[i]
+            else:
+                x = 0
+            arr.append(x + y)
+            i += 1
     if arr:
         return (arr[0], arr[1])
     return (0, 0)
+
+print(add_tuple((1,), (1,)))
